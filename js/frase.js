cf026d1ad5241a120ab5d0894d71863d51e0c6c7
Gabriel Alves <alves.gabriel7@gmail.com>
2022-09-20 08:47:46 -0300

    Requisicao ajax, enviando um objeto javaScript.

--- js/frase.js
+++ js/frase.js
@@ -1,4 +1,5 @@
 $("#button-Frase").click(fraseAleatoria);
+$("#button-FraseId").click(buscaFrase);
 function fraseAleatoria(){
     $("#spinner").toggle();
     $.get("http://localhost:3000/frases", trocaFrase)
@@ -20,5 +21,26 @@ function trocaFrase(data){
     AtualizaFrase();
     atualizaTempoInicia(data[numeroAleatorio].tempo);
 }
-
+function buscaFrase(){
+    $("#spinner").toggle();
+    var fraseId= $("#Frase-Id").val();
+    console.log(fraseId);
+    var dados = {id: fraseId}
+    $.get("http://localhost:3000/frases", dados,BuscaFrase)
+    .fail(function (){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2500)
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    })
+}
+function BuscaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    AtualizaFrase();
+    atualizaTempoInicia(data.tempo);
+}
 
